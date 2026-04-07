17d8892b01556fc8f36af0dda863d63ada03aec4
cutehusky <liyihong102@gmail.com>
2026-04-07 16:24:42 +0700

    refactor: implement IWriteRecord interface in IInsertRecord and IUpdateRecord for consistency

--- Drizzle4Dotnet/src/Core/Query/Insert/IInsertRecord.cs
+++ Drizzle4Dotnet/src/Core/Query/Insert/IInsertRecord.cs
@@ -1,8 +1,8 @@
 using Drizzle4Dotnet.Core.Schema.Tables;
+using Drizzle4Dotnet.Core.Shared;
 
 namespace Drizzle4Dotnet.Core.Query.Insert;
 
-public interface IInsertRecord<TTable> where TTable: ITable
+public interface IInsertRecord<TTable>: IWriteRecord where TTable: ITable
 {
-    public void Writer(Dictionary<string, object?> values);
 }
--- Drizzle4Dotnet/src/Core/Query/Update/IUpdateRecord.cs
+++ Drizzle4Dotnet/src/Core/Query/Update/IUpdateRecord.cs
@@ -1,6 +1,8 @@
+using Drizzle4Dotnet.Core.Schema.Tables;
+using Drizzle4Dotnet.Core.Shared;
+
 namespace Drizzle4Dotnet.Core.Query.Update;
 
-public interface IUpdateRecord<TTable>
+public interface IUpdateRecord<TTable>: IWriteRecord where TTable : ITable
 { 
-    public void Writer(Dictionary<string, object?> values);
 }
